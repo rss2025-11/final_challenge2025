0f8f25b054e81042bb82e5b5c911780f25d5c2d2
Suchitha <suchitha.channa@gmail.com>
2025-04-29 02:50:30 +0000

    Update lane detection math to find closest vertical line

--- track_segmentation/track_segmentation/hough_transform.py
+++ track_segmentation/track_segmentation/hough_transform.py
@@ -43,15 +43,15 @@ def edges_clean(image):
     cropped_dst = thinned_mask[len(thinned_mask[0])//5:, :]
     # cropped_dst = mask[len(mask[0])//5:, :]
 
-    # Convert to BGR for visualization
-    linesP = cv.HoughLinesP(cropped_dst, 1, np.pi / 180, 50, None, 100, 50) # TODO: Tune these numbers if needed
+    # Convert to BGR for visualization # was last 50
+    linesP = cv.HoughLinesP(cropped_dst, 1, np.pi / 180, 50, None, 100, 200) # TODO: Tune these numbers if needed
 
     # # For visualization only:
     cdstP_vis = cv.cvtColor(cropped_dst, cv.COLOR_GRAY2BGR)
-    # if linesP is not None:
-    #     for i in range(0, len(linesP)):
-    #         l = linesP[i][0]
-    #         cv.line(cdstP_vis, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv.line(cdstP_vis, (l[0], l[1]), (l[2], l[3]), (255,0,0), 1, cv.LINE_AA)
 
     # cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP_vis)
     # cv.waitKey()
@@ -60,7 +60,7 @@ def edges_clean(image):
 
 
 
-def compute_lane_edges(lines, image_width, image):
+def compute_lane_edges(lines, image_width, image_height, image):
     # lines shape: (N, 1, 4), where 4 = (x1, y1, x2, y2)
     lines = np.squeeze(lines)  # (N, 4)
 
@@ -70,7 +70,12 @@ def compute_lane_edges(lines, image_width, image):
     y1 = lines[:, 1]
     y2 = lines[:, 3]
 
-    score = np.abs(image_width/2 - (-x1 * np.divide((y2 - y1), (x2 - x1)) + y1))
+    bottom_image_y = image_height
+    slope = np.divide(y1-y2, x1-x2)
+    # intercept_to_robot describes the x intercept at the bottom of the image
+    # i.e. where vertically does the line reach the camera?
+    intercept_to_robot = (np.divide(bottom_image_y - y1, slope) + x1)
+    score = np.abs(image_width/2 - intercept_to_robot)
     max_index = np.argmin(score)
     closest_line = lines[max_index]
 
@@ -83,10 +88,22 @@ def compute_lane_edges(lines, image_width, image):
     cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", image)
     cv.waitKey()
     return 0
-    return closest_lines  # shape (2, 4)
+    return closest_line  # shape (2, 4)
 
-filename = "/home/racecar/racecar_ws/src/final_challenge2025/racetrack_images/lane_3/image16.png"
+def determine_midline(closest_line):
+    # convert using homography matrix
+    # determine if the closest line was to the left or right
+    # create a parallel line
+    pass
+
+def determine_lookahead_point(traj_line):
+    # choose a lookahead point that is some fixed distance away for now
+    # After testing with static lookahead distance, try reducing lookahead at bends
+        # lookahead distance = minumum(baseline, end of line segment)
+    pass
+
+filename = "/home/racecar/racecar_ws/src/final_challenge2025/racetrack_images/lane_1/image5.png"
 src = cv.imread(filename, cv.IMREAD_COLOR) # (Color, since later you cvtColor it to HSV)
 cdstP, linesP = edges_clean(src)
-compute_lane_edges(linesP, src.shape[1], cdstP)
+closest_line = compute_lane_edges(linesP, src.shape[1], src.shape[0], cdstP)
 # edges_clean(src)
