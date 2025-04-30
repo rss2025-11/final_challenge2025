bc7df9e7f51ec6f50e85395bcaed3d1276ca85ea
ppatel22 <73558351+ppatel22@users.noreply.github.com>
2025-04-30 16:44:28 -0400

    include new launch files and config files in setup, as well as the node files

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+import glob
 
 package_name = 'final_challenge2025'
 
@@ -10,6 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/final_challenge2025/launch/sim', glob.glob(os.path.join('launch', 'sim', '*launch.*'))),
+        ('share/final_challenge2025/launch/real', glob.glob(os.path.join('launch', 'real', '*launch.*'))),
+        # Include all config files
+        (os.path.join('share', package_name, 'config', 'sim'), glob.glob('config/sim/*.yaml')),
+        (os.path.join('share', package_name, 'config', 'real'), glob.glob('config/real/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +27,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'state_machine = final_challenge2025.state_machine:main',
+            'pure_pursuit = final_challenge2025.pure_pursuit:main',
+            'basement_point_publisher = final_challenge2025.basement_point_publisher:main',
         ],
     },
 )
